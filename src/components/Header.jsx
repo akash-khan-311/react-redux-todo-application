cf8e4a5da64da3983e98ba0ead505216bf28c5ea
Akash Khan <akashcc48@gmail.com>
2024-02-08 21:38:21 +0600

    save todo on server using thunk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@ import noteImage from '../assets/images/notes.png'
 import tickImg from '../assets/images/double-tick.png'
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { added, allCompleted, clearCompleted } from '../redux/todos/actions';
+import {  allCompleted, clearCompleted } from '../redux/todos/actions';
+import addTodo from '../redux/todos/thunk/addTodo';
 export default function Header() {
   const dispatch = useDispatch();
   const [input ,setInput] = useState('');
@@ -11,7 +12,7 @@ export default function Header() {
   }
   const handleSubmit = e=> {
     e.preventDefault();
-    dispatch(added(input))
+    dispatch(addTodo(input))
     setInput('')
 
   }
